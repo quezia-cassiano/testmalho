9d0e22b21e8bbf6b04c5351a61b56cc715f8245f
quezia-cassiano <105313051+quezia-cassiano@users.noreply.github.com>
2023-05-08 22:59:04 -0300

    finalmente

--- simple_server/app.js
+++ simple_server/app.js
@@ -19,6 +19,6 @@ app.get('/', function(req, res){
    res.send("Salve, Rapaziada!");
 });
 
-app.listen(3000,function(){
-    console.log('Listening on port 3000!');
+app.listen(4000,function(){
+    console.log('Listening on port 4000!');
 });
